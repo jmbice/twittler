179cbaccd671d8e113d8fa81d1985a0c5f41064c
Jordan Bice <jmbice@gmail.com>
2018-05-14 03:42:48 -0700

    Rewrite multiple redundant functions into single
    Write function that refreshes correct feed based on input, instead
    of individual functions.

--- assets/javascript/load_tweets.js
+++ assets/javascript/load_tweets.js
@@ -1,15 +1,16 @@
-var douglasDisplayLength = 0;
-var mracusDisplayLength = 0;
-var sharkDisplayLength = 0;
-var shawnDisplayLength = 0;
-var currentCount = 0;
-var snippet = ' • '
 var dateFormat = 'dddd, h:mm a'
-var cakeDisplayLength = 0;
 var cakeTweets = [];
+var tweetCounts = {
+  douglascalhoun: 0,
+  cake: 0,
+  mracus: 0,
+  shawndrost: 0,
+  sharksforcheap: 0,
+  feed: 0,
+};
 
 // writes your tweets
-var generateUserTweets = function(tweetText){
+var myTweets = function(tweetText){
   var tweet = {};
   tweet.user = 'cake';
   tweet.message = tweetText;
@@ -23,108 +24,47 @@ var loadFeed = function(){
   for (var j = 0; j < streams.home.length; j++){
     var tweet = streams.home[j];
     var $tweet = $('<div class="individual-tweets"></div>');
-    $tweet.text('@' + tweet.user + tweet.message + snippet + moment(tweet.created_at).format(dateFormat) + ' •');
+    $tweet.text('@' + tweet.user + tweet.message + ' • ' + moment(tweet.created_at).format(dateFormat) + ' •');
     $tweet.prependTo($('#feed'));
-    currentCount++;
+    tweetCounts.feed += 1;
   };
   formatTweets();
 };
 
-// refresh home feed
-var refreshFeed = function(){
-  var currentLength = streams.home.length;
-  if (currentLength > currentCount){
-    for (var j = currentCount; j < currentLength; j++){
-      var tweet = streams.home[j];
+// refresh feeds based on handle parameter (feed, douglascalhoun, cake, mracus, shawndrost, sharksforcheap)
+var refreshFeed = function(handle){
+  var destination = '#' + handle;
+  var source;
+
+  if (handle === 'feed'){
+    source = streams.home;
+  } else if (handle === 'cake'){
+    source = cakeTweets;
+  } else {
+    source = streams.users[handle];
+  }
+
+  if (source.length > tweetCounts[handle]){
+    for (var j = tweetCounts[handle]; j < source.length; j++){
+      var tweet = source[j];
       var $tweet = $('<div class="individual-tweets"></div>');
-      $tweet.text('@' + tweet.user + tweet.message + snippet + moment(tweet.created_at).format(dateFormat) + ' •');
-      $tweet.prependTo($('#feed'));
+      $tweet.text('@' + tweet.user + tweet.message + ' • ' + moment(tweet.created_at).format(dateFormat) + ' •');
+      $tweet.prependTo($(destination));
     }
-  currentCount = currentLength;
+  tweetCounts[handle] = source.length;
   }
   formatTweets();
 }
 
-// load or refresh individual feeds... douglas, cake, mracus, shawn, shark.
-var douglasFeed = function(){
-  var currentLength = streams.users.douglascalhoun.length;
-  if (currentLength > douglasDisplayLength){
-    for (var j = douglasDisplayLength; j < currentLength; j++){
-      var tweet = streams.users.douglascalhoun[j];
-      var $tweet = $('<div class="individual-tweets"></div>');
-      $tweet.text('@' + tweet.user + tweet.message + snippet + moment(tweet.created_at).format(dateFormat) + ' •');
-      $tweet.prependTo($('#douglas'))
-    }
-    douglasDisplayLength = currentLength;
-  }
-  formatTweets();
-}
 
-var cakeFeed = function(){
-  console.log(cakeTweets)
-  var currentLength = cakeTweets.length;
-  if (currentLength > cakeDisplayLength){
-    for (var j = cakeDisplayLength; j < currentLength; j++){
-      var tweet = cakeTweets[j];
-      var $tweet = $('<div class="individual-tweets"></div>');
-      $tweet.text('@' + tweet.user + tweet.message + snippet + moment(tweet.created_at).format(dateFormat) + ' •');
-      $tweet.prependTo($('#cake'))
-    }
-    newCakeTweets = currentLength;
-  }
-  formatTweets();
-}
-
-var mracusFeed = function(){
-  var currentLength = streams.users.mracus.length;
-  if (currentLength > mracusDisplayLength){
-    for (var j = mracusDisplayLength; j < currentLength; j++){
-      var tweet = streams.users.mracus[j];
-      var $tweet = $('<div class="individual-tweets"></div>');
-      $tweet.text('@' + tweet.user + tweet.message + snippet + moment(tweet.created_at).format(dateFormat) + ' •');
-      $tweet.prependTo($('#mracus'));
-    }
-    mracusDisplayLength = currentLength;
-  }
-  formatTweets();
-}
-
-var sharkFeed = function(){
-  var currentLength = streams.users.sharksforcheap.length;
-  if (currentLength > sharkDisplayLength){
-    for (var j = sharkDisplayLength; j < currentLength; j++){
-      var tweet = streams.users.sharksforcheap[j];
-      var $tweet = $('<div class="individual-tweets"></div>');
-      $tweet.text('@' + tweet.user + tweet.message + snippet + moment(tweet.created_at).format(dateFormat) + ' •');
-      $tweet.prependTo($('#shark'))
-    }
-    sharkDisplayLength = currentLength;
-  }
-  formatTweets();
-}
-
-var shawnFeed = function(){
-  var currentLength = streams.users.shawndrost.length;
-  if (currentLength > shawnDisplayLength){
-    for (var j = shawnDisplayLength; j < currentLength; j++){
-      var tweet = streams.users.shawndrost[j];
-      var $tweet = $('<div class="individual-tweets"></div>');
-      $tweet.text('@' + tweet.user + tweet.message + snippet + moment(tweet.created_at).format(dateFormat) + ' •');
-      $tweet.prependTo($('#shawn'))
-    }
-    shawnDisplayLength = currentLength;
-  }
-  formatTweets();
-}
-
-// Format tweet a
+// Format tweet handles and times
 var formatTweets = function(){
-  $(".tweets-wrapper:contains('@douglascalhoun')").html(function(_, html) {
-    return  html.replace(/@douglascalhoun/g, "<a id='douglas-link' class='links'>@ douglascalhoun: </a>")
-  });
-
   $(".tweets-wrapper:contains('@cake')").html(function(_, html) {
     return  html.replace(/@cake/g, "<a id='cake-link' class='links'>@ cake: </a>")
+  });
+
+  $(".tweets-wrapper:contains('@douglascalhoun')").html(function(_, html) {
+    return  html.replace(/@douglascalhoun/g, "<a id='douglas-link' class='links'>@ douglascalhoun: </a>")
   });
 
   $(".tweets-wrapper:contains('@mracus')").html(function(_, html) {
@@ -142,5 +82,4 @@ var formatTweets = function(){
   $(".tweets-wrapper:contains(' • ')").html(function(_, html) {
     return  html.replace(/\•.*?( pm •| am •)/g, "<a id='time'>$&</a>")
   });
-
 }
